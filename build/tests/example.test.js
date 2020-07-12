528c584079e98bb6552b7d9e0ed274135a827efc
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-07-12 16:48:43 +0300

    End-to-end GetCount with Build

--- build/tests/example.test.js
+++ build/tests/example.test.js
@@ -25,23 +25,24 @@ describe('Mocha steps demo', function () {
 
     (0, _mochaSteps.step)("should load google homepage", async function () {
         await page.goto("http://zero.webappsecurity.com/");
-        var signInButton = await page.isElementVisible("#signin_button");
-        (0, _chai.expect)(signInButton).to.be.true;
+        (0, _chai.expect)((await page.isElementVisible("#signin_button"))).to.be.true;
     });
 
     (0, _mochaSteps.step)("should display login form", async function () {
         await page.waitAndClick("#signin_button");
-        var loginForm = await page.isElementVisible("#login_form");
-        (0, _chai.expect)(loginForm).to.be.true;
-        var signInButton = await page.isElementVisible("#signin_button");
-        (0, _chai.expect)(signInButton).to.be.false;
+        (0, _chai.expect)((await page.isElementVisible("#login_form"))).to.be.true;
+        (0, _chai.expect)((await page.isElementVisible("#signin_button"))).to.be.false;
     });
 
     (0, _mochaSteps.step)("should login to application", async function () {
         await page.waitAndType("#user_login", "username");
         await page.waitAndType("#user_password", "password");
         await page.waitAndClick(".btn-primary");
-        var navbar = await page.isElementVisible(".nav-tabs");
-        (0, _chai.expect)(navbar).to.be.true;
+        (0, _chai.expect)((await page.isElementVisible(".nav-tabs"))).to.be.true;
+    });
+
+    (0, _mochaSteps.step)("should have 6 navber links", async function () {
+        var navbarLinksCount = await page.getCount(".nav-tabs li");
+        (0, _chai.expect)(navbarLinksCount).to.equal(6);
     });
 });
--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -17,23 +17,24 @@ describe('Mocha steps demo', () => {
 
     step("should load google homepage", async () => {
         await page.goto("http://zero.webappsecurity.com/");
-        const signInButton = await page.isElementVisible("#signin_button");
-        expect(signInButton).to.be.true
+        expect(await page.isElementVisible("#signin_button")).to.be.true
         });
     
     step ("should display login form", async () => {
         await page.waitAndClick("#signin_button");
-        const loginForm = await page.isElementVisible("#login_form");
-        expect(loginForm).to.be.true
-        const signInButton = await page.isElementVisible("#signin_button");
-        expect(signInButton).to.be.false
+        expect(await page.isElementVisible("#login_form")).to.be.true
+        expect(await page.isElementVisible("#signin_button")).to.be.false
     });
 
     step ("should login to application", async () => {
         await page.waitAndType("#user_login", "username");
         await page.waitAndType("#user_password", "password");
         await page.waitAndClick(".btn-primary");
-        const navbar = await page.isElementVisible(".nav-tabs");
-        expect(navbar).to.be.true;
+        expect(await page.isElementVisible(".nav-tabs")).to.be.true;
     });
+
+    step("should have 6 navbar links", async () => {
+        const navbarLinksCount = await page.getCount(".nav-tabs li");
+        expect(navbarLinksCount).to.equal(6);
+    })
 });
