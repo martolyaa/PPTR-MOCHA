df181434d7323ce595b5afad73f30361526869e0
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-07-12 13:55:55 +0300

    First Try Mocha-steps

--- build/tests/example.test.js
+++ build/tests/example.test.js
@@ -1,7 +1,40 @@
-"use strict";
+'use strict';
 
-describe("Config test", function () {
-    it("should work", function () {
-        console.log("Yeay");
+var _puppeteer = require('puppeteer');
+
+var _puppeteer2 = _interopRequireDefault(_puppeteer);
+
+var _mochaSteps = require('mocha-steps');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+describe('Mocha steps demo', function () {
+    var browser = void 0;
+    var page = void 0;
+
+    before(async function () {
+        browser = await _puppeteer2.default.launch({ headless: true });
+        page = await browser.newPage();
+        await page.setDefaultTimeout(7000);
+    });
+
+    after(async function () {
+        await browser.close();
+    });
+
+    (0, _mochaSteps.step)("should load google homepage", async function () {
+        await page.goto("https://www.google.com");
+    });
+
+    (0, _mochaSteps.step)("step 2 should fail", async function () {
+        await page.waitForSelector("#Fail");
+    });
+
+    (0, _mochaSteps.step)("step 3", async function () {
+        console.log("From step 3");
+    });
+
+    (0, _mochaSteps.step)("step 4", async function () {
+        console.log("From step 4");
     });
 });
--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -1,5 +1,33 @@
-describe("Config test", () => {
-    it("should work", () => {
-        console.log("Yeay");
+import puppeeteer from 'puppeteer';
+import { step } from "mocha-steps";
+
+describe('Mocha steps demo', () => {
+    let browser;
+    let page;
+
+    before(async () => {
+        browser = await puppeeteer.launch({ headless: true });
+        page = await browser.newPage();
+        await page.setDefaultTimeout(7000);
     });
-});
+
+    after(async () => {
+        await browser.close();
+    });
+
+    step("should load google homepage", async () => {
+        await page.goto("https://www.google.com");
+        });
+
+    step("step 2 should fail", async () => {
+        await page.waitForSelector("#Fail");
+    });
+
+    step("step 3", async () => {
+        console.log("From step 3");
+    });
+
+    step("step 4", async () => {
+        console.log("From step 4");
+    });
+})
